fa96e8296c9510e23106c78716123217eb996dbc
Yaroslav Kravchenko <yaroslavkravch@gmail.com>
2020-05-17 00:43:14 +0300

    Init commit

---
+++ flaskblog.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Yaroslav Kravchenko',
+        'title': 'Post number 1',
+        'content': 'First post content',
+        'date_posted': "May 16, 2020"
+    },
+    {
+        'author': 'Oleh Kononenko',
+        'title': 'Post number 2',
+        'content': 'Second post content',
+        'date_posted': "May 17, 2020"
+    }
+
+]
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
