8e7a15ff6656e5edf5b27017f114c14a48c340cd
Yaroslav Kravchenko <yaroslavkravch@gmail.com>
2020-05-17 12:18:06 +0300

    Make login/registration logic

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm
+from forms import LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '3273357638792F423F4528482B4D6251'
 
 posts = [
     {
@@ -28,6 +32,27 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash("Account created for {}!".format(form.username.data), "success")
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
